622b4b3a56b09e2d35400b228da03e478c4ccdc1
basnikrilova <sergey.arnaut.05@gmail.com>
2025-05-25 11:34:46 +0200

    executable jar

--- src/main/java/com/data_management/Patient.java
+++ src/main/java/com/data_management/Patient.java
@@ -52,6 +52,13 @@ public class Patient {
      *         range
      */
     public List<PatientRecord> getRecords(long startTime, long endTime) {
-        // TODO Implement and test this method
+        List<PatientRecord> recordsInRange = new ArrayList<>();
+        for (PatientRecord record : this.patientRecords) {
+            long ts = record.getTimestamp();
+            if (ts >= startTime && ts <= endTime) {
+                recordsInRange.add(record);
+            }
+        }
+        return recordsInRange;
     }
 }
